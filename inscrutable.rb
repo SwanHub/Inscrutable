bf7b650d4aeeeadd5e83747deb8640db09f03a43
SwanHub <jdprince555@gmail.com>
2019-06-18 02:46:40 -0600

    outline of gameplay, written in comments

---
+++ inscrutable.rb
@@ -0,0 +1,21 @@
+############ WELCOME TO THE GAME: ################
+################ INSCRUTABLE #####################
+
+# Plan:
+
+## Create methods for each "move" a player or cpu can do...
+## shift // jump // reverse // swap and their various forms... ()
+
+## have the computer apply four of these moves to the ordered sequence in a
+## randoml fashion.
+
+## have the user choose one move at a time, while recording how many moves
+## have been made.
+
+## if the user gets the tiles all back in working order by move 4, then they win.
+## if not, they can concede defeat and try again, concede defeat and quit,
+## or concede defeat and continue until the game is over.
+
+## I want to do some work with YIELD in this round. I also want to graduate
+## from do...end and move into the more ubiquitous and professional realm of {}.
+## I want this code to be written in 100 lines or less. 
