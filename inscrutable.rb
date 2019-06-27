c6b905dee12df011a07e453046b1ec177d7a288e
SwanHub <jdprince555@gmail.com>
2019-06-27 02:47:09 -0300

    methods complete, tied to ops doc

--- inscrutable.rb
+++ inscrutable.rb
@@ -1,54 +1,7 @@
-require 'pry'
+############ THE TIE-TOGETHER of the PROGRAM ############
+require './inscrutable_methods.rb'
+$current_board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-############ WELCOME TO THE GAME: ################
-################ INSCRUTABLE #####################
+display_board
 
-current_board = [4, 3, 2, 1, 5, 6, 7, 8, 9]
-# indexes:      [0, 1, 2, 3, 4, 5, 6, 7, 8]
-
-# Display board.
-def display_board(array)
-  array.map! do |el|
-    el += 1
-  end
-  print array
-end
-
-# DONE
-# shift_direction :: -1 or +1, rep R or L. shift_count defines # of places moved.
-def shift(current_board, shift_direction, shift_count)
-  new_array = []
-  current_board.each_with_index do |el, i|
-      new_array.push(current_board[(i - (shift_count*shift_direction))%9])
-    end
-    print new_array
-end
-
-# DONE
-# simply use the .reverse function on the existing array.
-def reverse(current_board)
-  current_board.reverse!
-  print current_board
-end
-
-# integers relating to the index of the swapped pieces
-def single_swap(current_board, piece_1, piece_2)
-  #Algorithm: - save piece_1 index...
-            # - save piece_1 value...
-            # - save piece_2 index...
-            # - save piece_2 value...
-            # - set piece_1 index to piece 2 value...
-            # - set piece_2 index to piece 1 value...
-
-end
-
-def jump(mobile_piece_number, mobile_piece_start_index, number_of_jumps, jump_direction)
-  # Algorithm: - save the piece indexes...
-            #  - save the piece value...
-            #  - create an array of those pieces using .slice!...
-            #  - .insert the array at the piece index -+ jump_number, depending.
-            #  - flatten the array.
-
-end
-
-# 3. Define method flow.
+### NOW FOR THE GAME FLOW ###
---
+++ inscrutable_methods.rb
@@ -0,0 +1,39 @@
+# Display board.
+def display_board
+  print $current_board
+end
+
+# shift_direction :: -1 or +1, rep R or L. shift_count defines # of places moved.
+def shift(shift_direction, shift_count)
+  new_array = []
+  $current_board.each_with_index do |el, i|
+      new_array.push($current_board[(i - (shift_count*shift_direction))%9])
+    end
+    $current_board = new_array
+end
+
+# simply use the .reverse function on the existing array.
+def reverse
+  $current_board.reverse!
+end
+
+# integers relating to the index of the swapped pieces
+def single_swap(piece_1, piece_2)
+  index_1 = $current_board.index(piece_1)
+  index_2 = $current_board.index(piece_2)
+  $current_board[index_1] = piece_2
+  $current_board[index_2] = piece_1
+end
+
+# Number or adjacent numbers jump L or R by X places.
+def jump(starting_piece, num_pieces_moved, number_of_jumps, jump_direction)
+  starting_index = $current_board.index(starting_piece)
+  slice_of_array = $current_board.slice!(starting_index, num_pieces_moved)
+  new_index = starting_index + (number_of_jumps*jump_direction)
+    # correcting for negative index notation.
+    if new_index < 0
+      new_index -= 1
+    end
+  $current_board.insert(new_index, slice_of_array)
+  $current_board.flatten!
+end
