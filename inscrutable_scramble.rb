d2edb82b8da1ae71446117cd1ec439eb13537108
SwanHub <jdprince555@gmail.com>
2019-06-30 01:19:29 -0300

    minor tweaks, experimenting with storing methods that take arguments in arrays

--- inscrutable_scramble.rb
+++ inscrutable_scramble.rb
@@ -3,8 +3,11 @@
 require './inscrutable_methods.rb'
 #require './inscrutable.rb'
 
-def shift_method
-  shift(-1, rand(1..8))
+#john = [-1, 1].sample
+#jill = rand(1..8)
+yes = -1
+def shift_method(yes)
+  shift(yes, 2)
 end
 
 odd_array = [method(:shift_method)]
@@ -12,5 +15,5 @@ odd_array = [method(:shift_method)]
 #this works.
 odd_array[0].call
 
-[-1, 1].sample #chooses one or the other form -1 or 1... probably the way to go
-rand(-1..1) #then figure out a way to remove 0 as an option.
+#[-1, 1].sample #chooses one or the other form -1 or 1... probably the way to go
+#rand(-1..1) #then figure out a way to remove 0 as an option.
