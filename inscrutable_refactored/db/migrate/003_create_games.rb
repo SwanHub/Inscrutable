c0a72599c66a2f8c4bd8866c71bfc364410c6fae
Jackson Prince <47403119+SwanHub@users.noreply.github.com>
2019-07-27 14:28:55 -0600

    remove start_time, autogenerated w timestamps

--- inscrutable_refactored/db/migrate/003_create_games.rb
+++ inscrutable_refactored/db/migrate/003_create_games.rb
@@ -2,7 +2,6 @@ class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
       t.integer :match_id
-      t.datetime :start_time
       t.datetime :finish_time
       t.string :result
       t.integer :remaining_moves
