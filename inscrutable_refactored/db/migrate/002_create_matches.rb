b7f4bdb63efb1bf2b1f0e2caf6cb811cb7e338e8
Jackson Prince <47403119+SwanHub@users.noreply.github.com>
2019-07-27 14:28:16 -0600

    remove start_time, autogenerated w timestamps

--- inscrutable_refactored/db/migrate/002_create_matches.rb
+++ inscrutable_refactored/db/migrate/002_create_matches.rb
@@ -2,7 +2,6 @@ class CreateMatches < ActiveRecord::Migration[5.2]
   def change
     create_table :matches do |t|
       t.integer :user_id
-      t.datetime :start_time
       t.datetime :finish_time
       t.timestamps
     end
