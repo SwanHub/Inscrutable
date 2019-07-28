544c2e3ab3f29b8229aedf828d87935e91584e8e
Jackson Prince <47403119+SwanHub@users.noreply.github.com>
2019-07-28 13:47:45 -0600

    delete

--- inscrutable_game/db/migrate/002_create_matches.rb
+++
@@ -1,9 +0,0 @@
-class CreateMatches < ActiveRecord::Migration[5.2]
-  def change
-    create_table :matches do |t|
-      t.integer :user_id
-      t.datetime :finish_time
-      t.timestamps
-    end
-  end
-end
